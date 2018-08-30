cdd1c5182ad08f983be644c01732ea922913af34
Arts <arts@point.im>
2018-08-30 23:45:22 +0300

    temporary checkbox action

--- web/src/components/record/Checklist/ChecklistItem.jsx
+++ web/src/components/record/Checklist/ChecklistItem.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Checkbox from 'react-toolbox/lib/checkbox';
 
-export default function NoteItem({
-  // $listId,
-  // $id,
-  checked,
-  text,
-}) {
-  return (
-    <li>
-      <Checkbox checked={checked} label={text} />
-    </li>
-  );
-}
+export default class NoteItem extends PureComponent {
+  static propTypes = {
+    // $listId: PropTypes.string.isRequired,
+    // $id: PropTypes.string.isRequired,
+    checked: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired,
+  };
 
-NoteItem.propTypes = {
-  // $listId: PropTypes.string.isRequired,
-  // $id: PropTypes.string.isRequired,
-  checked: PropTypes.bool.isRequired,
-  text: PropTypes.string.isRequired,
-};
+  constructor({ checked }) {
+    super();
+    this.state = { checked };
+  }
+
+  // TODO: action
+  toggleCheck = () => {
+    this.setState(({ checked }) => ({ checked: !checked }));
+  };
+
+  render() {
+    const { text } = this.props;
+    const { checked } = this.state;
+
+    return (
+      <li>
+        <Checkbox
+          checked={checked}
+          label={text}
+          onChange={this.toggleCheck}
+        />
+      </li>
+    );
+  }
+}
