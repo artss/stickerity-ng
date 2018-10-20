c8ff0a6c90b64ba951de41b5beff92984196240a
arts <arts@point.im>
2018-10-20 17:20:09 +0300

    Add list button

--- web/src/pages/Board/Board.jsx
+++ web/src/pages/Board/Board.jsx
@@ -2,9 +2,11 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
+import { Button } from 'react-toolbox/lib/button';
+import { FontIcon } from 'react-toolbox/lib/font_icon';
 
 import { listType } from '../../proptypes/list';
-
+import Sticker from '../../components/Sticker';
 import BoardItem from './BoardItem';
 
 import s from './Board.css';
@@ -22,6 +24,13 @@ class Board extends PureComponent {
         <Helmet>
           <title>My Board</title>
         </Helmet>
+
+        <Sticker className={s.addListButtonSticker}>
+          <Button className={s.addListButton} onClick={this.addList}>
+            <FontIcon value="add" className={s.addListButtonIcon} />
+            <div className={s.addListButtonText}>Add list</div>
+          </Button>
+        </Sticker>
 
         {lists.map(list => (
           <BoardItem
