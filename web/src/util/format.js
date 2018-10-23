54a498d44b8c620f324427677810f41c7c0ef6cd
arts <arts@point.im>
2018-10-23 22:43:39 +0300

    Add/edit passwords

--- web/src/util/format.js
+++ web/src/util/format.js
@@ -13,3 +13,7 @@ export function firstWords(text, wordsNum) {
   const chunk = words.slice(0, wordsNum).join(' ').replace(/\s+\.+$/, '');
   return words.length > wordsNum ? `${chunk}...` : chunk;
 }
+
+export function formatUrl(url) {
+  return /^\w\/\//.test(url) ? url : `http://${url}`;
+}
