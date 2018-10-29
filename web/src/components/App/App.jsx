e2ff6b56597d484ec90d215b3b992535855c1035
arts <arts@point.im>
2018-10-29 08:58:31 +0300

    Master password form (incomplete)

--- web/src/components/App/App.jsx
+++ web/src/components/App/App.jsx
@@ -1,9 +1,8 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Header from '../Header';
+import MasterPasswordForm from '../MasterPasswordForm';
 
 import Board from '../../pages/Board';
 import AddList from '../../pages/AddList';
@@ -11,48 +10,26 @@ import EditList from '../../pages/EditList';
 import List from '../../pages/List';
 import AddItem from '../../pages/AddItem';
 import Item from '../../pages/Item';
-import MasterPasswordForm from '../../pages/MasterPasswordForm';
 
 import styles from './App.css';
 
-class App extends PureComponent {
-  static propTypes = {
-    masterPasswordAdded: PropTypes.bool,
-  };
+export default function App() {
+  return (
+    <div className={styles.app}>
+      <Header />
 
-  static defaultProps = {
-    masterPasswordAdded: false,
-  };
+      <main>
+        <Switch>
+          <Route path="/" exact component={Board} />
+          <Route path="/lists/add" exact component={AddList} />
+          <Route path="/lists/:listId" exact component={List} />
+          <Route path="/lists/:listId/edit" exact component={EditList} />
+          <Route path="/lists/:listId/add" exact component={AddItem} />
+          <Route path="/lists/:listId/:itemId" exact component={Item} />
+        </Switch>
+      </main>
 
-  render() {
-    const { masterPasswordAdded } = this.props;
-
-    return (
-      <div className={styles.app}>
-        <Header />
-
-        <main>
-          {masterPasswordAdded
-            ? (
-              <Switch>
-                <Route path="/" exact component={Board} />
-                <Route path="/lists/add" exact component={AddList} />
-                <Route path="/lists/:listId" exact component={List} />
-                <Route path="/lists/:listId/edit" exact component={EditList} />
-                <Route path="/lists/:listId/add" exact component={AddItem} />
-                <Route path="/lists/:listId/:itemId" exact component={Item} />
-              </Switch>
-            )
-            : <MasterPasswordForm />
-          }
-        </main>
-      </div>
-    );
-  }
+      <MasterPasswordForm />
+    </div>
+  );
 }
-
-function mapStateToProps({ user: { masterPasswordAdded } }) {
-  return { masterPasswordAdded };
-}
-
-export default connect(mapStateToProps)(App);
