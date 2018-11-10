556f04841d44fbd5583d2fbf4b8b63823fe67824
arts <arts@point.im>
2018-11-10 03:46:11 +0300

    env files

--- api/src/models/config.js
+++ api/src/models/config.js
@@ -2,11 +2,11 @@ const config = {};
 
 ['development', 'production'].forEach((key) => {
   config[key] = {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 5432),
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT || 5432),
+    database: process.env.POSTGRES_NAME,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
     dialect: 'postgres',
   };
 });
