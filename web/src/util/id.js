89216ceb27db72be964948074fc33fdf8e919b08
arts <arts@point.im>
2018-11-23 12:03:43 +0300

    ID generation fix

--- web/src/util/id.js
+++ web/src/util/id.js
@@ -1,6 +1,5 @@
 import { getTime } from './time';
 
 export function generateId() {
-  const time = getTime();
-  return btoa(time.toString(36)).replace(/[^a-z0-9-]+/ig, '');
+  return getTime().toString(36);
 }
